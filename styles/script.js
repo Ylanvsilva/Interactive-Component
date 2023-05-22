a4d372a3f85a2bb119714578452f2fcd96216134
Ylan <ylan_vinicio@hotmail.com>
2023-05-22 19:30:16 -0300

    Alterações basicas

--- styles/script.js
+++ styles/script.js
@@ -1,16 +1,92 @@
-const circleButton = document.querySelector('div#btn1');
+const circleButton1 = document.querySelector('div#btn1');
 
-circleButton.addEventListener('click', clicar)
+circleButton1.addEventListener('click', clicar)
 
 function clicar(color) {
     const button = document.querySelector('.circle_btn.circle_orange')
-    // console.log(button);
+    console.log(button);
     // console.log(color);
 
     //* Toggle
     if (button === null) {
         color.classList.add('circle_orange');
-        console.log('cheguei aq')
+        console.log('cheguei aq');
+    } else {
+        console.log('entrei no else')
+        button.classList.remove('circle_orange');
+    }
+}
+
+const circleButton2 = document.querySelector('div#btn2');
+
+circleButton2.addEventListener('click', clicar2)
+
+function clicar2(color) {
+    const button = document.querySelector('.circle_btn.circle_orange')
+    console.log(button);
+    // console.log(color);
+
+    //* Toggle
+    if (button === null) {
+        color.classList.add('circle_orange');
+        console.log('cheguei aq');
+    } else {
+        console.log('entrei no else')
+        button.classList.remove('circle_orange');
+    }
+}
+
+const circleButton3 = document.querySelector('div#btn3');
+
+circleButton3.addEventListener('click', clicar3)
+
+function clicar3(color) {
+    const button = document.querySelector('.circle_btn.circle_orange')
+    console.log(button);
+    // console.log(color);
+
+    //* Toggle
+    if (button === null) {
+        color.classList.add('circle_orange');
+        console.log('cheguei aq');
+    } else {
+        console.log('entrei no else')
+        button.classList.remove('circle_orange');
+    }
+}
+
+const circleButton4 = document.querySelector('div#btn4');
+
+circleButton4.addEventListener('click', clicar4)
+
+function clicar4(color) {
+    const button = document.querySelector('.circle_btn.circle_orange')
+    console.log(button);
+    // console.log(color);
+
+    //* Toggle
+    if (button === null) {
+        color.classList.add('circle_orange');
+        console.log('cheguei aq');
+    } else {
+        console.log('entrei no else')
+        button.classList.remove('circle_orange');
+    }
+}
+
+const circleButton5 = document.querySelector('div#btn5');
+
+circleButton5.addEventListener('click', clicar5)
+
+function clicar5(color) {
+    const button = document.querySelector('.circle_btn.circle_orange')
+    console.log(button);
+    // console.log(color);
+
+    //* Toggle
+    if (button === null) {
+        color.classList.add('circle_orange');
+        console.log('cheguei aq');
     } else {
         console.log('entrei no else')
         button.classList.remove('circle_orange');
