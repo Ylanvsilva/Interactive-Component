a9057bf9c47174e8eebb405dc903af764060cc3d
Ylan <ylan_vinicio@hotmail.com>
2023-07-31 21:20:15 -0300

    Ajustando JS

--- styles/app.js
+++ styles/app.js
@@ -3,13 +3,12 @@ const btns = document.querySelectorAll('.circle_btn')
 btns.forEach((btn) =>
     btn.addEventListener('click', function(e) {
         const button = (e.currentTarget.textContent)
-
-        if (button === '1') {
-            btn.classList.add("circle_orange")
-            console.log('if')
-        } else {
-            button.classList.remove("circle_orange")
-            console.log('else')
+        const buttonId = (this.id)
+        console.log(button)
+        console.log(buttonId)
+        
+        if (button === buttonId) {
+            btn.classList.add('circle_orange')
         }
     })
 )
